499407c4baaa84ea1b46c870567b9ea9dbfdde36
Masahiro Takata <takata005581016@gmail.com>
2020-03-22 23:54:18 +0900

    インクリメンタルサーチ【実装中】

--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,16 @@ Route::get('/', function () { // Laravelの初期画面
 
 Route::get('/shopping', 'ShoppingController@index'); // 商品一覧
 Route::get('/shopping/{id}', 'ShoppingController@show'); // 商品詳細
-
-Route::post('/shopping/cart', 'ShoppingController@cart'); // カートに保存
-
 Route::get('/shopping/cart', 'ShoppingController@cartcheck'); // カート
-
+//Route::get('/shopping/search', 'ShoppingController@search'); // 検索機能
+Route::match(['get', 'post'],'/shopping/search', 'ShoppingController@search'); // 検索機能
 Route::get('/users/register', 'UsersController@index'); // ユーザ新規登録
-Route::post('/users/confirm', 'UsersController@confirm'); // 登録確認（ユーザ）
-Route::post('/users/complete', 'UsersController@complete'); // ユーザ登録完了（コントローラー内で登録処理を行うのでgetを使う）
 Route::get('/users/login', 'UsersController@login'); // ログイン
 Route::get('/users/logout', 'UsersController@logout'); // ログアウト
-Route::post('/users/logincomplete', 'UsersController@logincomplete'); // ログイン完了
-
 Route::get('/buying/buyComfirm', 'BuyingController@index'); // 購入手続き
 Route::get('/buying/buyingComplete', 'BuyingController@buyingComplete'); // 購入完了
+
+Route::post('/shopping/cart', 'ShoppingController@cart'); // カートに保存
+Route::post('/users/confirm', 'UsersController@confirm'); // 登録確認（ユーザ）
+Route::post('/users/complete', 'UsersController@complete'); // ユーザ登録完了（コントローラー内で登録処理を行うのでgetを使う）
+Route::post('/users/logincomplete', 'UsersController@logincomplete'); // ログイン完了
