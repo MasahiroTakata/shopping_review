c0c73bc31dd502727844e9ef9e68624ddec0afd7
Masahiro Takata <takata005581016@gmail.com>
2020-05-05 18:32:41 +0900

    本番環境対応

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/top.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/top.css') }}">
     @yield('css')
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script type="text/javascript" src="{{ asset('js/search.js') }}"></script>
