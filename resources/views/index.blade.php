cea0a3998d405b0ab66ef92c386aadf714ba0694
Masahiro Takata <takata005581016@gmail.com>
2021-07-10 21:20:28 +0900

    filtering-search
    フィルタリング機能（カテゴリー）の実装完了

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,23 +1,24 @@
 @extends('layouts.default')
 @section('title', 'Shopping Review')
 @section('css')
-<link rel="stylesheet" href="{{ secure_asset('css/imagehover.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/imagehover.min.css') }}">
 @endsection
 @section('content')
+@include('submenu', ['categorys' => $submenu])
 @if (isset ($products))
 <div class = 'content'>
-    @foreach ($products as $product)
-      <figure class="imghvr-fade">
-        <img src="{{ $product->image }}" class="productImage">
-        <figcaption>
-          カテゴリー：{{ $product->category->name }}<br/><br/>
-          商品名：{{ $product->name }}<br/><br/>
-          価格：{{ $product->price }}円
-        </figcaption>
-        <a href="{{ action('ShoppingController@show', $product->id) }}"></a>
-      </figure>
-    @endforeach
-    {{ $products->links() }}
+  @foreach ($products as $product)
+    <figure class="imghvr-fade">
+      <img src="{{ $product->image }}" class="productImage">
+      <figcaption>
+        カテゴリー：{{ $product->category->name }}<br/><br/>
+        商品名：{{ $product->name }}<br/><br/>
+        価格：{{ $product->price }}円
+      </figcaption>
+      <a href="{{ action('ShoppingController@show', $product->id) }}"></a>
+    </figure>
+  @endforeach
+  {{ $products->links() }}
 </div>
 @endif
 @endsection
